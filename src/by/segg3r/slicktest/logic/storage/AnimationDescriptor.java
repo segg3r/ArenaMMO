05fefed8fc66c7ce32e2e6b3f509d3f1ca065b91
dede322 <dede3223232@mail.ru>
2014-11-02 19:44:20 +0400

    AnimationStorage и AnimationDescriptor
    реализовал метод
    public List<AnimationDescriptor> getAnimationDescriptors()
    
    добавил переменные в класс AnimationDescriptor :
    offsetX - сдвиг центра картинки по X;
    offsetY - сдвиг центра картинки по Y;
    imageSpeed - скорость смены кадров в анимации;

--- src/by/segg3r/slicktest/logic/storage/AnimationDescriptor.java
+++ src/by/segg3r/slicktest/logic/storage/AnimationDescriptor.java
@@ -5,12 +5,43 @@ public class AnimationDescriptor {
 	private String fileName;
 	private int columns;
 	private int rows;
+	private int offsetX;
+	private int offsetY;
+	private double imageSpeed = 1;
 
-	public AnimationDescriptor(String fileName, int columns, int rows) {
+	public double getImageSpeed() {
+		return imageSpeed;
+	}
+
+	public void setImageSpeed(double imageSpeed) {
+		this.imageSpeed = imageSpeed;
+	}
+
+	public int getOffsetX() {
+		return offsetX;
+	}
+
+	public void setOffsetX(int offsetX) {
+		this.offsetX = offsetX;
+	}
+
+	public int getOffsetY() {
+		return offsetY;
+	}
+
+	public void setOffsetY(int offsetY) {
+		this.offsetY = offsetY;
+	}
+
+	public AnimationDescriptor(String fileName, int columns, int rows,
+			int offsetX, int offsetY, double imageSpeed) {
 		super();
 		this.fileName = fileName;
 		this.columns = columns;
 		this.rows = rows;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.imageSpeed = imageSpeed;
 	}
 
 	public String getFileName() {
--- src/by/segg3r/slicktest/logic/storage/AnimationStorage.java
+++ src/by/segg3r/slicktest/logic/storage/AnimationStorage.java
@@ -1,5 +1,11 @@
 package by.segg3r.slicktest.logic.storage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -30,11 +36,46 @@ public class AnimationStorage {
 		}
 	}
 
-	private List<AnimationDescriptor> getAnimationDescriptors() {
-		//вернуть список описаний анимаций из файла в папке, путь к которой указан в path		
-		List<AnimationDescriptor> result = new ArrayList<AnimationDescriptor>();
-		result.add(new AnimationDescriptor("001-Fighter01", 4, 4));
-		return result;
+	public List<AnimationDescriptor> getAnimationDescriptors() 
+	{
+		//вернуть список описаний анимаций из файла в папке, путь к которой указан в path
+		try 
+		{
+			File file = new File(path + "\\animationDescriptor.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			
+			List<AnimationDescriptor> result = new ArrayList<AnimationDescriptor>();
+			String line;
+			while ((line = br.readLine()) != null)
+			{
+				String[] parametres;
+				parametres = line.split(";");
+				if (parametres[0].equals("*") != true)
+				{
+					result.add(new AnimationDescriptor(parametres[0], Integer.parseInt(parametres[1]), Integer.parseInt(parametres[2]), Integer.parseInt(parametres[3]), Integer.parseInt(parametres[4]), Double.parseDouble(parametres[5])));
+				}
+				else
+				{
+					File dir = new File(path);
+					File[] files = dir.listFiles();
+					for(File f : files)
+					{
+						if (f.getPath().matches(this.path.replaceAll("\\\\", "\\\\\\\\") + "\\\\.+\\.png"))
+						{
+							StringBuilder sb = new StringBuilder(f.getName());
+							sb.delete(f.getName().length() - 4, f.getName().length());
+							result.add(new AnimationDescriptor(sb.toString(), Integer.parseInt(parametres[1]), Integer.parseInt(parametres[2]), Integer.parseInt(parametres[3]), Integer.parseInt(parametres[4]), Double.parseDouble(parametres[5])));
+						}
+					}
+				}
+			}
+			
+			br.close();
+		
+			return result;
+		}
+		catch (Exception e)
+		{ return new ArrayList<AnimationDescriptor>(); }
 	}
 
 	private void createAnimation(String animationName, int columns, int rows) throws SlickException {
